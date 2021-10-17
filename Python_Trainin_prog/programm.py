c781c65f5f1e33972b33e7b083de75585083ba58
Mukund Murarka <39944614+Mukundmurarka@users.noreply.github.com>
2021-10-17 21:35:21 +0530

    day2 commit

--- Python_Trainin_prog/programm.py
+++ Python_Trainin_prog/programm.py
@@ -1,4 +1,4 @@
-# Write a code in python to determine a year is leap eyar not not ;
+# Write a code in python to determine a year is leap year not not ;
 
 year = int(input("enter the year"))
 if (year %4 ==0):
@@ -55,4 +55,38 @@ while num1 !=0:
     rev_num = rev_num*10 +dig
     num1 //=10
 print("reversed number is ",rev_num)
+
+
+#WAP to display fibonaci series upto given terms
+ given_num=int(input("enter the range of series"))
+ n1=0
+ n2=1
+ sum=0
+ count =1
+ if(given_num<0):
+     print("enter a positive number")
+ elif(given_num==1):
+     print("fibonaci series upto 1 term is :",n1)
+ else:
+     print("fibonaci series upto given term is :", end=" ")
+     while(count <=given_num):
+         print(sum,end=" ")
+         count = count+1
+         n1=n2
+         n2=sum
+         sum = n1+n2
+
+#WAP to calculate the cube of all the numner to a givn number
+ given_num = int(input("enter the number "))
+ for i in range (1,given_num+1):
+     cube = i**3
+     print(cube)
+
+#WAP to remove all the integers from a string
+
+import re
+string1 = input("enter a string")
+patr = r'[0-9]'
+new_str = re.sub(patr,"",string1)
+print(new_str)
 

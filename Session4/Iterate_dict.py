8b6dd510bae829bf7f841c9de9b92b7aeb3dfeaa
MukundKumarMurarka <mukund.kumar@tibilsolutions.com>
2021-10-20 22:29:44 +0530

    day 4 commiting

--- Session4/Iterate_dict.py
+++
@@ -1,9 +0,0 @@
-# 6. WAP to iterate over a dictionary using for loop
-def iterate_dict(mobile):
-
-    for i, j in mobile.items():
-        print(i, j)
-
-
-mobile={"rahul":"iphone", "john":"motorola", "peter":"ineplus","shyam":"samsung"}
-iterate_dict(mobile)
--- Session4/Reverse_list.py
+++
@@ -1,9 +0,0 @@
-# 3. WAP to Reverse List.
-
-def reverse_list(names):
-    new_list = names[::-1]
-    print("reversed list : ",new_list)
-n= int(input("enter the length of list : "))
-names =[x for x in input("enter the element of list : ").split()]
-print("original list : ",names)
-reverse_list(names)
